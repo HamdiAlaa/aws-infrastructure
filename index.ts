0e5a363eb224cb5218898b9cd6425b82f634ef7c
HamdiAlaa* <alaahamdi57@gmail.com>
2019-12-13 15:54:15 +0100

    replacement tag with type

--- index.ts
+++ index.ts
@@ -19,28 +19,26 @@ let ami = stackConfig.get('ami') || pulumi.output(aws.getAmi({
 })).apply(result => result.id);
 
 // create a new security group for port 80
-let securityGroup = new aws.ec2.SecurityGroup(`${__.params.tag}--scrtgrp`, {
+let securityGroup = new aws.ec2.SecurityGroup(`${__.params.type}--scrtgrp`, {
     ingress: [
         { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: ["0.0.0.0/0"] },
         { protocol: "tcp", fromPort: 80, toPort: 80, cidrBlocks: ["0.0.0.0/0"] },
     ],
 
 });
-const nodeKey = new aws.ec2.KeyPair(`${__.params.tag}--keyPair`, {
-    keyName: `${__.params.tag}-key-name`,
+const nodeKey = new aws.ec2.KeyPair(`${__.params.type}--keyPair`, {
+    keyName: `${__.params.type}-key-name`,
     publicKey: __.params.public_key
 });
 let userData =
-    `#!/bin/bash
-        echo "Hello, World it Me!" > index.html
-        nohup python -m SimpleHTTPServer 80 &` || stackConfig.get('script');
+    '' || stackConfig.get('script');
 //Begin LOOP
 for (let index = 1; index <= __.params.node_number; index++) {
-    let server = new aws.ec2.Instance(`${__.params.tag}--node--${index}`, {
+    let server = new aws.ec2.Instance(`${__.params.type}--node--${index}`, {
         instanceType: __size.getSize(),
         securityGroups: [securityGroup.name],
         ami: __.params.ami,
-        userData: userData,
+        //userData: userData,
         keyName: nodeKey.keyName
     });
     ipAddressesList.push(server.publicIp);
