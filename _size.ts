fa0bd2af2e785ccd5ba6d2cde397081ae11fe710
HamdiAlaa* <alaahamdi57@gmail.com>
2019-12-24 17:26:43 +0100

    after passing to database

--- _size.ts
+++ _size.ts
@@ -1,6 +1,6 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-const __ = require('../config/aws_nodes.json');
+const __ = new pulumi.Config();
 
 var size: pulumi.Input<aws.ec2.InstanceType>;
 
@@ -8,9 +8,9 @@ export function getSize(){
     // It must be switch condition
     // To choose the Vm size, in type script the vmSize fave aws.ec2.instanceTypes as a type not a string like js
 
-    if (__.params.node_size == 't2_micro') {size = aws.ec2.InstanceTypes.T2_Micro;}
-    if (__.params.node_size == 't2_medium') {size = aws.ec2.InstanceTypes.T2_Medium};
-    if (__.params.node_size == 't2_large') {size = aws.ec2.InstanceTypes.T2_Large;}
-    if (__.params.node_size == 't2_xlarge') {size = aws.ec2.InstanceTypes.T2_XLarge;}
+    if (__.require('node_size') == 't2_micro') {size = aws.ec2.InstanceTypes.T2_Micro;}
+    if (__.require('node_size') == 't2_medium') {size = aws.ec2.InstanceTypes.T2_Medium};
+    if (__.require('node_size') == 't2_large') {size = aws.ec2.InstanceTypes.T2_Large;}
+    if (__.require('node_size') == 't2_xlarge') {size = aws.ec2.InstanceTypes.T2_XLarge;}
     return size;
 }
--- index.ts
+++ index.ts
@@ -3,14 +3,13 @@ import * as aws from "@pulumi/aws";
 import { Output } from "@pulumi/pulumi";
 
 import * as __size from './_size';
-const __ = require('../config/aws_nodes.json');
-const stackConfig = new pulumi.Config();
+const __ = new pulumi.Config();
 let ipAddressesList: Output<string>[] = [];
 let dnsList: Output<string>[] = [];
-
+let public_key:string = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCU3nkTvuHaCSJ2uKE8KWo23+5JqUG8VokNKlXjrWA2DxK9ioNpKVJ8nnCoEHz1Qr6MwhDG9rN5e3fwCbYVDRtTeFBP01bKCzRdAhvixu3xDqvyPnMYdTeOmj2CuTNKngdRfXzgMIqzyQPkn3ZC/WSPsbQb44RBzRnZYbSxPCAhj4soOu5EWcWK8xPzK4ukBniUknNeD8CZ7GRgwNsheF2rATFLGEy/wuGwn+nJlfSlQtKfeBrEF76TDgcpCOVTwp4rdAp1PWiCh0RLgaXfRUbYlx5lrapNaSRuzfrlkuqXUedjs6vEM/BgAp7KYHDjJF+fABxZBZLhMPPRZW8NA94J alaa@alaa";
 
 // Get the id for the latest Amazon Linux AMI
-let ami = stackConfig.get('ami') || pulumi.output(aws.getAmi({
+let ami = __.get('ami') || pulumi.output(aws.getAmi({
     filters: [
         { name: "name", values: ["amzn-ami-hvm-*-x86_64-ebs"] },
     ],
@@ -19,25 +18,25 @@ let ami = stackConfig.get('ami') || pulumi.output(aws.getAmi({
 })).apply(result => result.id);
 
 // create a new security group for port 80
-let securityGroup = new aws.ec2.SecurityGroup(`${__.params.type}--scrtgrp`, {
+let securityGroup = new aws.ec2.SecurityGroup(`${__.get('type')}--scrtgrp`, {
     ingress: [
         { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: ["0.0.0.0/0"] },
         { protocol: "tcp", fromPort: 80, toPort: 80, cidrBlocks: ["0.0.0.0/0"] },
     ],
 
 });
-const nodeKey = new aws.ec2.KeyPair(`${__.params.type}--keyPair`, {
-    keyName: `${__.params.type}-key-name`,
-    publicKey: __.params.public_key
+const nodeKey = new aws.ec2.KeyPair(`${__.get('type')}--keyPair`, {
+    keyName: `${__.get('type')}-key-name`,
+    publicKey: public_key
 });
-let userData =
-    '' || stackConfig.get('script');
+// let userData =
+//     '' || __.require('script');
 //Begin LOOP
-for (let index = 1; index <= __.params.node_number; index++) {
-    let server = new aws.ec2.Instance(`${__.params.type}--node--${index}`, {
+for (let index = 1; index <= +__.require('node_number'); index++) {
+    let server = new aws.ec2.Instance(`${__.get('type')}--node--${index}`, {
         instanceType: __size.getSize(),
         securityGroups: [securityGroup.name],
-        ami: __.params.ami,
+        ami: ami,
         //userData: userData,
         keyName: nodeKey.keyName
     });
